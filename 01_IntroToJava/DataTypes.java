f76848bf2cba853cfe4b9f98d5229a2edbd4ff8e
shivansh2501 <sbansal.official@gmail.com>
2025-03-28 15:46:45 +0530

    Learning Datatypes

---
+++ 01_IntroToJava/DataTypes.java
@@ -0,0 +1,42 @@
+class DataTypes {
+
+    public static void main(String[] args) {
+
+        //boolean
+        boolean bt = true;
+        System.out.println(bt);
+
+        //byte
+        byte n;
+        n = 127;
+        System.out.println(n);
+
+        //char
+        char a = 'A';
+        char b = 66;
+        System.out.println(a);
+        System.out.println(b);
+
+        //short
+        short m = 1234;
+        System.out.println(m);
+
+        //int
+        int num1 = 56782;
+        System.out.println(num1);
+
+        //long
+        long num2 = 15000000000L;
+        System.out.println(num2);
+
+        //float
+        float num3 = 3.14f;
+        System.out.println(num3);
+
+        //double
+        double num4 = 69.784d; //whether d is there or not there is no difference
+        System.out.println(num4);
+
+    }
+
+}
